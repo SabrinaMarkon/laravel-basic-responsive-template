edeb1c5e89738e3b8b3f5649f0db7c15011f398c
Sabrina Markon <phpsitescripts@outlook.com>
2018-07-09 23:27:33 -0600

    Edit main route view

--- routes/web.php
+++ routes/web.php
@@ -11,6 +11,12 @@
 |
 */
 
+/*
+SABRINA:
+In PHP, anonymous functions are called "Closure". A Closure is a function that doesn't have a name.
+We use Closure to handle "routing" in small applications. In large applications, we use Controllers.
+It's recommended that you should always use Controllers because Controllers help to structure your code easier. For instance, you may group all user actions into UserController, all post actions into PostsController.
+*/
 Route::get('/', function () {
     return view('welcome');
 });
